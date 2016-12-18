52bd762b9bc3886fc3e7afef2546039c21e4963f
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-12-18 14:49:33 -0600

    integrate with redux

---
+++ src/components/CostInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CostInput extends Component {
+  render() {
+    return (
+      <div>
+        <span>Enter cost</span>
+        <input />
+      </div>
+    )
+  }
+}
+
+export default CostInput;
