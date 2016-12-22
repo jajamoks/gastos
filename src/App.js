bba0d9dddb816f2f1cfea041d6d3900d0cb72d5b
Jeffrey Huang <jeffreyrhuang@gmail.com>
2016-12-22 13:44:18 -0600

    setup firebase config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import firebase from 'firebase';
 import CostInput from './components/CostInput';
 import CostTable from './components/CostTable';
 import MonthSelector from './components/MonthSelector';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyBJPElyq7LJ3ZG5D07aGrmZtLypN_EYSuM",
+      authDomain: "gastos-25d0b.firebaseapp.com",
+      databaseURL: "https://gastos-25d0b.firebaseio.com",
+      storageBucket: "gastos-25d0b.appspot.com",
+      messagingSenderId: "276626258741"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Grid>
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
   RESET_COST,
   SAVE_COST,
